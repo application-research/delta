6a5e2fe9a3657e42244862457f5ff46898b39804
alvin-reyes <me@areyes.onl>
2023-02-15 17:54:20 -0500

    spammer script

--- scripts/spammer.sh
+++ scripts/spammer.sh
@@ -6,7 +6,7 @@ do
   ms=$(date +%s%N)
   dd if=/dev/random of=random_"$ms".dat bs=10000 count=250000
 	curl --location --request POST 'http://localhost:1414/api/v1/content/add' \
-  --header 'Authorization: Bearer EST18096aa7-e0cc-4a2e-8a03-e595fe534b14ARY' \
+  --header 'Authorization: Bearer [ESTUARY_API_KEY]' \
   --form 'data=@"./random_'${ms}'.dat"' \
   --form 'connection_mode="online"' \
   rm -rf random_$ms.dat
