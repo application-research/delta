f07487dd2743864b7f90954274c364f99d95d8af
alvin-reyes <me@areyes.onl>
2023-03-16 19:18:12 -0400

    added instance uuid endpoint

--- api/node.go
+++ api/node.go
@@ -14,6 +14,7 @@ func ConfigureNodeInfoRouter(e *echo.Group, node *core.DeltaNode) {
 
 	nodeGroup := e.Group("/node")
 	nodeGroup.GET("/info", handleNodeInfo(node))
+	nodeGroup.GET("/uuids", handleNodeUuidInfo(node))
 	nodeGroup.GET("/addr", handleNodeAddr(node))
 	nodeGroup.GET("/peers", handleNodePeers(node))
 	nodeGroup.GET("/host", handleNodeHost(node))
@@ -83,6 +84,31 @@ func handleNodeAddr(node *core.DeltaNode) func(c echo.Context) error {
 	}
 }
 
+type Uuids struct {
+	ID           uint   `json:"id"`
+	InstanceUuid string `json:"instance_uuid"`
+	CreatedAt    string `json:"created_at"`
+}
+
+// It returns a function that returns a JSON response with the node's name, description, and type
+func handleNodeUuidInfo(node *core.DeltaNode) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		_, span := otel.Tracer("handleNodeUuidInfo").Start(context.Background(), "handleNodeUuidInfo")
+		defer span.End()
+
+		span.SetName("handleNodeUuidInfo")
+		span.SetAttributes(attribute.String("user-agent", c.Request().UserAgent()))
+		span.SetAttributes(attribute.String("path", c.Path()))
+		span.SetAttributes(attribute.String("method", c.Request().Method))
+
+		// instance meta
+		var uuids []Uuids
+		node.DB.Model(&db_models.InstanceMeta{}).Scan(&uuids)
+
+		return c.JSON(200, uuids)
+	}
+}
+
 // It returns a function that returns a JSON response with the node's name, description, and type
 func handleNodeInfo(node *core.DeltaNode) func(c echo.Context) error {
 	return func(c echo.Context) error {
@@ -98,15 +124,10 @@ func handleNodeInfo(node *core.DeltaNode) func(c echo.Context) error {
 		nodeDescription := node.Config.Node.Description
 		nodeType := node.Config.Node.Type
 
-		// instance meta
-		var instanceMeta db_models.InstanceMeta
-		node.DB.Model(&db_models.InstanceMeta{}).First(&instanceMeta)
-
 		return c.JSON(200, map[string]string{
 			"name":        nodeName,
 			"description": nodeDescription,
 			"type":        nodeType,
-			"uuid":        instanceMeta.InstanceUuid,
 		})
 	}
 }
