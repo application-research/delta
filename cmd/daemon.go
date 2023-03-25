a7a93893943c02b455738cb4573a7cf23c685e2d
alvin-reyes <me@areyes.onl>
2023-03-24 21:29:21 -0400

    24 hour clean up time per node

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -4,6 +4,7 @@ import (
 	"delta/api"
 	c "delta/config"
 	"delta/core"
+	"delta/jobs"
 	"delta/utils"
 	"fmt"
 	"github.com/application-research/delta-db/db_models"
@@ -138,9 +139,9 @@ func DaemonCmd(cfg *c.DeltaConfig) []*cli.Command {
 			fmt.Println(utils.Blue + "Subscribing the event listeners... DONE" + utils.Reset)
 
 			// run the clean up every 30 minutes so we can retry and also remove the unecessary files on the blockstore.
-			fmt.Println(utils.Blue + "Running the atomatic cron jobs" + utils.Reset)
+			fmt.Println(utils.Blue + "Running the atomic cron jobs" + utils.Reset)
 			RunScheduledCleanupAndRetryCron(ln)
-			fmt.Println(utils.Blue + "Running the atomatic cron jobs... DONE" + utils.Reset)
+			fmt.Println(utils.Blue + "Running the atomic cron jobs... DONE" + utils.Reset)
 
 			// launch the API node
 			fmt.Println(utils.Blue + "Starting Delta." + utils.Reset)
@@ -201,12 +202,13 @@ func RunScheduledCleanupAndRetryCron(ln *core.DeltaNode) {
 	maxCleanUpJobs := ln.Config.Dispatcher.MaxCleanupWorkers
 
 	s := gocron.NewScheduler()
-	s.Every(30).Minutes().Do(func() { // let's clean and retry every 30 minutes. It'll only get the old data.
+	s.Every(24).Hour().Do(func() { // let's clean and retry every 30 minutes. It'll only get the old data.
 		dispatcher := core.CreateNewDispatcher()
 		//dispatcher.AddJob(jobs.NewItemContentCleanUpProcessor(ln))
-		//dispatcher.AddJob(jobs.NewRetryProcessor(ln))
+		dispatcher.AddJob(jobs.NewRetryProcessor(ln))
 		dispatcher.Start(maxCleanUpJobs)
 
+		core.CleanUpContentAndPieceComm(ln)
 		core.ScanHostComputeResources(ln, ln.Node.Config.Blockstore)
 	})
 
