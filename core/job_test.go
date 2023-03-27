e4044ecf90915052d67401cc6f20b928dea8c2bf
alvin-reyes <me@areyes.onl>
2023-03-26 22:59:36 -0400

    add unit tests

--- core/job_test.go
+++ core/job_test.go
@@ -1,199 +1,104 @@
 package core
 
 import (
-	"reflect"
+	"errors"
+	"sync"
 	"testing"
 )
 
-func TestCreateNewDispatcher(t *testing.T) {
-	tests := []struct {
-		name string
-		want *Dispatcher
-	}{
-		// TODO: Add test cases.
+// MockProcessor is a mock implementation of the IProcessor interface
+type MockProcessor struct {
+	err error
+}
+
+func (m *MockProcessor) Run() error {
+	return m.err
+}
+
+func TestDispatcher(t *testing.T) {
+	// Create a new dispatcher
+	d := CreateNewDispatcher()
+
+	// Add some jobs to the dispatcher
+	numJobs := 5
+	var wg sync.WaitGroup
+	for i := 0; i < numJobs; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			d.AddJob(&MockProcessor{})
+		}(i)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CreateNewDispatcher(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CreateNewDispatcher() = %v, want %v", got, tt.want)
+
+	// Start the dispatcher with two workers
+	d.Start(2)
+
+	// Wait for all jobs to finish
+	wg.Wait()
+
+	// Check that there were no errors processing the jobs
+	if !d.Finished() {
+		t.Errorf("Dispatcher did not finish all jobs")
+	}
+
+	// Check that the dispatchStatus channel received the expected messages
+	var workerQuitCount, jobCount int
+	for {
+		select {
+		case ds := <-d.dispatchStatus:
+			if ds.Type == "worker" && ds.Status == "quit" {
+				workerQuitCount++
 			}
-		})
+			if ds.Type == "job" && ds.Status == "done" {
+				jobCount++
+			}
+			if workerQuitCount == 2 && jobCount == numJobs {
+				// Expected number of messages received
+				return
+			}
+		default:
+			// No more messages in dispatchStatus channel
+			t.Errorf("Did not receive expected dispatch status messages")
+			return
+		}
 	}
 }
 
-func TestCreateNewWorker(t *testing.T) {
-	type args struct {
-		id          int
-		workerQueue chan *Worker
-		jobQueue    chan *Job
-		dStatus     chan *DispatchStatus
-	}
-	tests := []struct {
-		name string
-		args args
-		want *Worker
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CreateNewWorker(tt.args.id, tt.args.workerQueue, tt.args.jobQueue, tt.args.dStatus); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CreateNewWorker() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+func TestJobWithError(t *testing.T) {
+	// Create a new dispatcher
+	d := CreateNewDispatcher()
 
-func TestDispatcher_AddJob(t *testing.T) {
-	type fields struct {
-		jobCounter     int
-		jobQueue       chan *Job
-		dispatchStatus chan *DispatchStatus
-		workQueue      chan *Job
-		workerQueue    chan *Worker
-	}
-	type args struct {
-		je IProcessor
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				jobCounter:     tt.fields.jobCounter,
-				jobQueue:       tt.fields.jobQueue,
-				dispatchStatus: tt.fields.dispatchStatus,
-				workQueue:      tt.fields.workQueue,
-				workerQueue:    tt.fields.workerQueue,
-			}
-			d.AddJob(tt.args.je)
-		})
-	}
-}
+	// Add a job with an error to the dispatcher
+	err := errors.New("job error")
+	d.AddJob(&MockProcessor{err})
 
-func TestDispatcher_AddJobAndDispatch(t *testing.T) {
-	type fields struct {
-		jobCounter     int
-		jobQueue       chan *Job
-		dispatchStatus chan *DispatchStatus
-		workQueue      chan *Job
-		workerQueue    chan *Worker
-	}
-	type args struct {
-		je         IProcessor
-		numWorkers int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				jobCounter:     tt.fields.jobCounter,
-				jobQueue:       tt.fields.jobQueue,
-				dispatchStatus: tt.fields.dispatchStatus,
-				workQueue:      tt.fields.workQueue,
-				workerQueue:    tt.fields.workerQueue,
-			}
-			d.AddJobAndDispatch(tt.args.je, tt.args.numWorkers)
-		})
-	}
-}
+	// Start the dispatcher with one worker
+	d.Start(1)
 
-func TestDispatcher_Finished(t *testing.T) {
-	type fields struct {
-		jobCounter     int
-		jobQueue       chan *Job
-		dispatchStatus chan *DispatchStatus
-		workQueue      chan *Job
-		workerQueue    chan *Worker
+	// Check that the dispatcher finished with an error
+	if !d.Finished() {
+		t.Errorf("Dispatcher did not finish all jobs")
 	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				jobCounter:     tt.fields.jobCounter,
-				jobQueue:       tt.fields.jobQueue,
-				dispatchStatus: tt.fields.dispatchStatus,
-				workQueue:      tt.fields.workQueue,
-				workerQueue:    tt.fields.workerQueue,
-			}
-			if got := d.Finished(); got != tt.want {
-				t.Errorf("Finished() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestDispatcher_Start(t *testing.T) {
-	type fields struct {
-		jobCounter     int
-		jobQueue       chan *Job
-		dispatchStatus chan *DispatchStatus
-		workQueue      chan *Job
-		workerQueue    chan *Worker
-	}
-	type args struct {
-		numWorkers int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				jobCounter:     tt.fields.jobCounter,
-				jobQueue:       tt.fields.jobQueue,
-				dispatchStatus: tt.fields.dispatchStatus,
-				workQueue:      tt.fields.workQueue,
-				workerQueue:    tt.fields.workerQueue,
+	// Check that the dispatchStatus channel received the expected messages
+	var workerQuitCount, jobCount int
+	for {
+		select {
+		case ds := <-d.dispatchStatus:
+			if ds.Type == "worker" && ds.Status == "quit" {
+				workerQuitCount++
 			}
-			d.Start(tt.args.numWorkers)
-		})
-	}
-}
-
-func TestWorker_Start(t *testing.T) {
-	type fields struct {
-		ID             int
-		jobs           chan *Job
-		dispatchStatus chan *DispatchStatus
-		Quit           chan bool
-	}
-	tests := []struct {
-		name   string
-		fields fields
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			w := &Worker{
-				ID:             tt.fields.ID,
-				jobs:           tt.fields.jobs,
-				dispatchStatus: tt.fields.dispatchStatus,
-				Quit:           tt.fields.Quit,
+			if ds.Type == "job" && ds.Status == "error" {
+				jobCount++
 			}
-			w.Start()
-		})
+			if workerQuitCount == 1 && jobCount == 1 {
+				// Expected number of messages received
+				return
+			}
+		default:
+			// No more messages in dispatchStatus channel
+			t.Errorf("Did not receive expected dispatch status messages")
+			return
+		}
 	}
 }
